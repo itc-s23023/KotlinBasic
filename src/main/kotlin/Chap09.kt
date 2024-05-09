df756670e68551ded55f936ebf393f931897a5a7
s23023 <s23023@std.it-college.ac.jp>
2024-05-09 09:56:56 +0900

    9章終わり

---
+++ src/main/kotlin/Chap09.kt
@@ -0,0 +1,14 @@
+package org.example
+
+fun main(args: Array<String>) {
+    var a: Int = 1
+    a = 2 + 1
+    println(a)
+    var b: Int = 100
+    b += 1
+    b -= 1
+    b *= 3
+    b /= 10
+    b %= 10
+    println(b)
+}
