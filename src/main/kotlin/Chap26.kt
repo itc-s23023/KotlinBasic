be094109b59e1cb8a4241a6486569c4f6ec1884e
itc-s23023 <s23023@std.it-college.ac.jp>
2024-05-14 14:54:29 +0900

    26章追加（終わり）

--- src/main/kotlin/Chap26.kt
+++ src/main/kotlin/Chap26.kt
@@ -1,7 +1,7 @@
 package org.example
 
 fun main(args: Array<String>) {
-
+    val p = Person4(9)
 }
 
 class Person4(val name: String, var age: Int) {
@@ -9,5 +9,7 @@ class Person4(val name: String, var age: Int) {
         println("Called secondary constructor's initializer")
         println("Called parameter is ${name} and ${age}")
     }
-    constructor(age:Int) : this("Unknown", age)
+    constructor(age:Int) : this("Unknown", age) {
+        println("Called secondary constructor's initializer that only select age")
+    }
 }
